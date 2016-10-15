df70da39302573d7df6ac7e6ac819569c87ba24b
pinnamanivenkat <pinnamanivenkat@gmail.com>
2016-10-15 07:52:13 +0530

    Setup files added

--- cm.mk
+++ cm.mk
@@ -2,6 +2,7 @@ $(call inherit-product, device/lenovo/aio_otfp/device_aio_otfp.mk)
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := aio_otfp
 PRODUCT_NAME := cm_aio_otfp
 PRODUCT_BRAND := Lenovo
