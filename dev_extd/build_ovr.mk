2a9e8352d7c9d096cd7d1af198a8cb6e45df6c73
pinnamanivenkat <pinnamanivenkat@gmail.com>
2016-11-23 11:46:20 +0530

    Unrecognised lines removed

--- dev_extd/build_ovr.mk
+++ dev_extd/build_ovr.mk
@@ -1,18 +1,3 @@
-ADDITIONAL_DEFAULT_PROPERTIES += \
-     ro.adb.secure=0 \
-     ro.secure=0 \
-     ro.allow.mock.location=1 \
-     ro.debuggable=1 \
-     persist.service.adb.enable=1
-     
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
-     ro.zygote=zygote64_32 \
-     camera.disable_zsl_mode=1 \
-     ro.mount.fs=EXT4 \
-     persist.service.acm.enable=0 \
-     persist.sys.usb.config=mtp,mass_storage,adb \
-     persist.sys.timezone=Asia/Kolkata
-
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.product.locale.language=en \
     ro.product.locale.region=IN \
@@ -23,12 +8,11 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.telephony.ril.config=fakeiccid  \
     persist.call_recording.enabled=true \
     persist.call_recording.src=4 \
-    persist.debug.wfd.enable=1
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    	dalvik.vm.boot-dex2oat-threads=4 \
-    	dalvik.vm.dex2oat-threads=2 \
-    	dalvik.vm.image-dex2oat-threads=4
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=8
+    persist.debug.wfd.enable=1 \
+    ro.secure=0 \
+    ro.adb.secure=0 \
+    ro.debuggable=1 \
+    persist.sys.usb.config=mtp,adb \
+    persist.service.adb.enable=1 \
+    persist.service.debuggable=1
 
